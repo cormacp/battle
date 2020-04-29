194f2767d24425e83e96b632195cd704ab83728d
Cormac Phelan <cormacphelan@gmail.com>
2020-04-29 17:12:25 +0100

    Adds authentication module

---
+++ auth.py
@@ -0,0 +1,22 @@
+from flask import request
+from flask import Response
+from functools import wraps
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def requires_simple_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        logging.info(f"Authorising API request with simple auth")
+        auth_token = request.headers.get('Authorization')
+        if auth_token != "admin":
+            logging.info(f"Unauthorising request")
+            return Response(
+                json.dumps({"Access denied": "Invalid auth token"}),
+                status=401, mimetype='application/json')
+        logging.info(f"Request authorized")
+        return f(*args, **kwargs)
+    return decorated
